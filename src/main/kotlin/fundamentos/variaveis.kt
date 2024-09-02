1b17b3b1b9ea53b5f3f133a67afb1dc07ddc5516
FilipeTavaresR <filipertavares9@gmail.com>
2024-09-02 20:04:32 -0300

    fun dizOi(nome: String, idade: Int){

---
+++ src/main/kotlin/fundamentos/variaveis.kt
@@ -0,0 +1,20 @@
+package org.example.fundamentos
+
+fun main() {
+    var nome = "Filipe"
+    val nomeVal = "Filipe"
+
+    nome = "José"
+    nomeVal = "José"
+
+    val idade = 34
+    println(idade)
+}
+
+class variaveis{
+    lateinit var teste: String
+
+    fun iniciaVariaveis(){
+        teste = "Teste"
+    }
+}
