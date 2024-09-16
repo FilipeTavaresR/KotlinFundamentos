ea49f561a2193b10f4990486518c5ccbf17404a1
FilipeTavaresR <filipertavares9@gmail.com>
2024-09-16 07:29:50 -0300

    retorno de funções

---
+++ src/main/kotlin/fundamentos/return_funcoes.kt
@@ -0,0 +1,16 @@
+package org.example.fundamentos
+
+fun retornaNumeroPorExtenso(numero: Int): String{
+    if(numero == 5){
+        return "Cinco"
+    } else if(numero == 6){
+        return "Seis"
+    }
+    return "Número não mapeado"
+}
+
+fun main() {
+    println(retornaNumeroPorExtenso(5))
+    println(retornaNumeroPorExtenso(6))
+    println(retornaNumeroPorExtenso(12))
+}
